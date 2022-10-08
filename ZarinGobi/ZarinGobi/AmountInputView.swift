4da1c2f88da97496f08a43c178d71e8fa26a5a15
bee712 <48711413+bee712@users.noreply.github.com>
2022-10-08 15:00:55 +0900

    Create AmountInputView.swift

---
+++ ZarinGobi/ZarinGobi/AmountInputView.swift
@@ -0,0 +1,20 @@
+//
+//  AmountInputView.swift
+//  ZarinGobi
+//
+//  Created by Eunbi Han on 2022/10/08.
+//
+
+import SwiftUI
+
+struct AmountInputView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct AmountInputView_Previews: PreviewProvider {
+    static var previews: some View {
+        AmountInputView()
+    }
+}
