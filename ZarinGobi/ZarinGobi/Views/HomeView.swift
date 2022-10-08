b2b3964da2c1939fb8f11659325f572cf03b8347
YunSeok-Choi <dbstjr9898@naver.com>
2022-10-08 17:00:10 +0900

    feat/TitleTextUI 생성

--- ZarinGobi/ZarinGobi/Views/HomeView.swift
+++ ZarinGobi/ZarinGobi/Views/HomeView.swift
@@ -11,7 +11,26 @@ import CoreData
 struct HomeView: View {
     var body: some View {
         NavigationView {
-            Text("Hello")
+            VStack{
+                HStack{
+                    VStack(alignment: .leading){
+                        Text("자린고비가")
+                        Text("알려준다")
+                    }
+                    .font(.system(size: 48, weight: .bold))
+                    .padding(31)
+                    
+                    Spacer()
+                    
+                    //TODO: 매달린 자린고비 Image
+                    //Image(named: ??)
+                }
+                
+                //TODO: - 자린고비 이미지
+                //Image(named: ??)
+                
+                
+            }
         }
     }
 }
