027d471312542aa359a56a6564700c3165940f41
bee712 <48711413+bee712@users.noreply.github.com>
2022-10-08 17:56:19 +0900

    Create TextField for UserInput

--- ZarinGobi/ZarinGobi/Views/AmountInputView.swift
+++ ZarinGobi/ZarinGobi/Views/AmountInputView.swift
@@ -12,6 +12,8 @@ struct AmountInputView: View {
     let testProductNames: [String] = ["햇반 (210g)", "오뚜기밥 (210g)"]
     let testProductImages: [String] = ["rice1", "rice2"]
     
+    @State var productCount: String = ""
+    
     
     
     var body: some View {
@@ -35,12 +37,33 @@ struct AmountInputView: View {
                     }
                 }
                 
-                Image(systemName: "heart")
+                Image("")
                     .resizable()
                     .frame(width: 209, height: 209)
                     .background(.gray)
                     .padding(.top, 52)
-                    
+                
+                HStack {
+                    TextField(text: $productCount, label: {
+                        Text("개수가 몇개냐?")
+                            .foregroundColor(.gray)
+                    })
+                    .frame(width: 124, height: 24)
+                    .keyboardType(.decimalPad)
+                    Text("개")
+                        .padding(.leading, 7)
+                }.padding(.top, 68)
+                
+                HStack {
+                    TextField(text: $productCount, label: {
+                        Text("얼마냐?")
+                            .foregroundColor(.gray)
+                    })
+                    .frame(width: 124, height: 24)
+                    .keyboardType(.decimalPad)
+                    Text("원")
+                        .padding(.leading, 7)
+                }.padding(.top, 48)
                 
                 
                 
