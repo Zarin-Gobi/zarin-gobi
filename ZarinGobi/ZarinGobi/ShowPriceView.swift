7eb21945039e6191fad39a5380ef715fdfe5744e
bee712 <48711413+bee712@users.noreply.github.com>
2022-10-08 15:01:48 +0900

    Create ShowPriceView.swift

---
+++ ZarinGobi/ZarinGobi/ShowPriceView.swift
@@ -0,0 +1,20 @@
+//
+//  ShowPriceView.swift
+//  ZarinGobi
+//
+//  Created by Eunbi Han on 2022/10/08.
+//
+
+import SwiftUI
+
+struct ShowPriceView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct ShowPriceView_Previews: PreviewProvider {
+    static var previews: some View {
+        ShowPriceView()
+    }
+}
