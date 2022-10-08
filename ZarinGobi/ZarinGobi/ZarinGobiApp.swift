1ec731fea5a8b89e7844e08617f6658727fde811
bee712 <48711413+bee712@users.noreply.github.com>
2022-10-08 14:57:58 +0900

    Modify ContentView name to HomeView

--- ZarinGobi/ZarinGobi/ZarinGobiApp.swift
+++ ZarinGobi/ZarinGobi/ZarinGobiApp.swift
@@ -13,7 +13,7 @@ struct ZarinGobiApp: App {
 
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            HomeView()
                 .environment(\.managedObjectContext, persistenceController.container.viewContext)
         }
     }
