31b5219a9efaae81018f5577469b206696e3e904
bee712 <48711413+bee712@users.noreply.github.com>
2022-10-09 14:22:18 +0900

    modify customNavigationBar

--- ZarinGobi/ZarinGobi/Views/AmountInputView.swift
+++ ZarinGobi/ZarinGobi/Views/AmountInputView.swift
@@ -11,6 +11,7 @@ struct AmountInputView: View {
     
     let testProductNames: [String] = ["햇반 (210g)", "오뚜기밥 (210g)"]
     let testProductImages: [String] = ["rice1", "rice2"]
+    let categoryTitle: String = "즉석밥"
     
     @State var userInputProductCount: String = ""
     @State var userInputProductPrice: String = ""
@@ -20,62 +21,70 @@ struct AmountInputView: View {
     
     
     var body: some View {
-        ScrollView {
-            LazyVStack(pinnedViews: [.sectionHeaders]) {
-                Section(header:
-                            VStack {
-                    ScrollView(.horizontal, showsIndicators: false) {
-                        HStack {
-                            SelectProductButton(productNames: testProductNames)
-                        }
-                        .padding(1)
-                        .background(.white)
-                        .overlay(
-                            GeometryReader { geo in
-                                Color.clear.onAppear {
-                                    contentSize = geo.size
-                                    if contentSize.width >= UIScreen.main.bounds.width {
-                                        shouldScroll = true
-                                    }
-                                }
-                            })
-                        
-                    }
-                    .simultaneousGesture(DragGesture(minimumDistance: shouldScroll ? 20 : 0), including: .all)
-                    .frame(maxWidth: contentSize.width, maxHeight: 64)
-                }
-                    .frame(width: UIScreen.main.bounds.width, height: 64, alignment: .center)
-                    .position(x: 0 + UIScreen.main.bounds.width/2, y: 32)
-                ) {
-                    Image("")
-                        .resizable()
-                        .frame(width: 240, height: 240)
-                        .background(.gray)
-                        .padding(.top, 12)
-                    
-                    MakeUserInputTextField(productCount: $userInputProductCount, textFieldHint: "개수가 몇개냐?", textFieldUnit: "개").padding(.top, 32)
-                    
-                    MakeUserInputTextField(productCount: $userInputProductPrice, textFieldHint: "얼마냐?", textFieldUnit: "원").padding(.top, 61)
-                    
-                    
-                    Button(action: {
-                        
-                    }, label: {
-                        Text("결과 보기")
-                            .frame(minWidth: 0, maxWidth: .infinity, minHeight: 56,maxHeight: 56, alignment: .center)
-                            .foregroundColor(.white)
-                    })
-                    .background(.gray)
-                    .cornerRadius(12)
-                    .padding([.leading, .trailing], 16)
-                    
-                }
-                
-                
-            }
-            .navigationTitle("test").navigationBarTitleDisplayMode(.inline)
-            
+        
+        VStack {
+            customNavigationBar(testProductNames: testProductNames, categoryTitle: categoryTitle, shouldScroll: $shouldScroll, contentSize: $contentSize)
+            Divider()
         }
+        .position(x: UIScreen.main.bounds.width/2, y: 53)
+            .navigationBarHidden(true)
+        
+//        ScrollView {
+//            LazyVStack(pinnedViews: [.sectionHeaders]) {
+//                Section(header:
+//                            VStack {
+//                    ScrollView(.horizontal, showsIndicators: false) {
+//                        HStack {
+//                            SelectProductButton(productNames: testProductNames)
+//                        }
+//                        .padding(1)
+//                        .background(.white)
+//                        .overlay(
+//                            GeometryReader { geo in
+//                                Color.clear.onAppear {
+//                                    contentSize = geo.size
+//                                    if contentSize.width >= UIScreen.main.bounds.width {
+//                                        shouldScroll = true
+//                                    }
+//                                }
+//                            })
+//
+//                    }
+//                    .simultaneousGesture(DragGesture(minimumDistance: shouldScroll ? 20 : 0), including: .all)
+//                    .frame(maxWidth: contentSize.width, maxHeight: 64)
+//                }
+//                    .frame(width: UIScreen.main.bounds.width, height: 64, alignment: .center)
+//                    .position(x: 0 + UIScreen.main.bounds.width/2, y: 32)
+//                ) {
+//                    Image("")
+//                        .resizable()
+//                        .frame(width: 240, height: 240)
+//                        .background(.gray)
+//                        .padding(.top, 12)
+//
+//                    MakeUserInputTextField(productCount: $userInputProductCount, textFieldHint: "개수가 몇개냐?", textFieldUnit: "개").padding(.top, 32)
+//
+//                    MakeUserInputTextField(productCount: $userInputProductPrice, textFieldHint: "얼마냐?", textFieldUnit: "원").padding(.top, 61)
+//
+//
+//                    Button(action: {
+//
+//                    }, label: {
+//                        Text("결과 보기")
+//                            .frame(minWidth: 0, maxWidth: .infinity, minHeight: 56,maxHeight: 56, alignment: .center)
+//                            .foregroundColor(.white)
+//                    })
+//                    .background(.gray)
+//                    .cornerRadius(12)
+//                    .padding([.leading, .trailing], 16)
+//
+//                }
+//
+//
+//            }
+//            .navigationTitle("test").navigationBarTitleDisplayMode(.inline)
+//
+//        }
     }
     
     
@@ -140,22 +149,37 @@ struct SelectProductButton: View {
     }
 }
 
-
+// 커스텀 네비게이션 바 & 세부 상품 선택 버튼
 struct customNavigationBar: View {
     let testProductNames: [String]
+    let categoryTitle: String
     
     @Binding var shouldScroll: Bool
     @Binding var contentSize: CGSize
     
+    @Environment(\.presentationMode) var mode: Binding<PresentationMode>
+    
     var body: some View {
         VStack {
-            HStack {
-                Image(systemName: "arrow")
-                Spacer()
-                Text("즉석밥")
-                Spacer()
-                Spacer()
-            }.frame(width: .infinity, height: 44)
+            ZStack {
+                HStack {
+                    Button(action: {
+                        self.mode.wrappedValue.dismiss()
+                    }, label: {
+                        Image(systemName: "chevron.backward")
+                            .font(.system(size: 24,weight: .medium))
+                            .foregroundColor(.black)
+                            .padding(.leading, 8)
+                            .padding([.bottom, .top], 10)
+                    })
+                    Spacer()
+                    Spacer()
+                }
+                
+                Text("\(categoryTitle)")
+                    .font(.system(size: 17, weight: .semibold))
+                    .padding([.top, .bottom], 11)
+            }
             
             VStack {
                 ScrollView(.horizontal, showsIndicators: false) {
@@ -180,7 +204,7 @@ struct customNavigationBar: View {
             }
             .frame(width: UIScreen.main.bounds.width, height: 64, alignment: .center)
             
-        }.frame(width: .infinity, height: 107)
+        }.frame(width: UIScreen.main.bounds.width, height: 107)
         
     }
 }
