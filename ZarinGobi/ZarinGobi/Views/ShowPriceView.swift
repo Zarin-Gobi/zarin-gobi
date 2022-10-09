9879fd4effbe698381e23eb607a1e30e72a24c0d
dasol <hurdasol@naver.com>
2022-10-09 20:16:29 +0900

    [Feat]ShowPriceView 커스텀 네비게이션 바 추가 및 레이아웃 조절

--- ZarinGobi/ZarinGobi/Views/ShowPriceView.swift
+++ ZarinGobi/ZarinGobi/Views/ShowPriceView.swift
@@ -23,148 +23,155 @@ struct ShowPriceView: View {
             // 화면 width와 height 잡기
             let viewWidth = geo.size.width
             let viewHeight = geo.size.height
-            // MARK: - 전체 V스택
-            VStack(alignment: .center){
-                
-                // MARK: - 1번째 부분
-                VStack(alignment: .leading, spacing: viewHeight*0.023){
-                    // 가격 기준 설명
-                    Text("이 물품의 시세는 1개 당...")
-                        .foregroundColor(.descriptionGray)
-                        .font(.system(size: 16, weight: .medium))
-                    
-                    // 가격
-                    HStack(alignment: .center, spacing: viewWidth*0.15){
-                        // 최소가격
-                        VStack(alignment: .leading, spacing: 14){
-                            Text("최소")
-                                .font(.custom("ChosunCentennial", size: 24))
-                            
-                            Text("\(tempLowPrice)원")
-                                .font(.custom("ChosunCentennial", size: 36))
-                                .background( GeometryReader { geo in
-                                    Path { path in
-                                        path.move(to: CGPoint(x: 0, y: geo.size.height-5))
-                                        path.addLine(to: CGPoint(x: geo.size.width, y: geo.size.height-5))
-                                    }
-                                    .stroke(style: StrokeStyle(lineWidth: 20)) // adjust to your liking
-                                    .foregroundColor(.underBarGray)
-                                })
-                            
-                        }
-                        // 최대가격
-                        VStack(alignment: .leading,spacing: 14){
-                            Text("최대")
-                                .font(.custom("ChosunCentennial", size: 24))
-                            
-                            
-                            Text("\(tempHighPrice)원")
-                                .font(.custom("ChosunCentennial", size: 36))
-                                .background( GeometryReader { geo in
-                                    Path { path in
-                                        path.move(to: CGPoint(x: 0, y: geo.size.height-5))
-                                        path.addLine(to: CGPoint(x: geo.size.width, y: geo.size.height-5))
-                                    }
-                                    .stroke(style: StrokeStyle(lineWidth: 20)) // adjust to your liking
-                                    .foregroundColor(.underBarGray)
-                                })
-                            
+            
+            VStack() {
+                CustomNavigationBar(categoryTitle: "상품 이름 넣기")
+                    .frame(width: viewWidth)
+                // MARK: - 전체 V스택
+                VStack(alignment: .leading) {
+                    // MARK: - 1번째 부분
+                    VStack(alignment: .leading, spacing: viewHeight*0.023){
+                        // 가격 기준 설명
+                        Text("이 물품의 시세는 1개 당...")
+                            .foregroundColor(.descriptionGray)
+                            .font(.system(size: 16, weight: .medium))
+                        
+                        // 가격
+                        HStack(alignment: .center, spacing: viewWidth*0.15){
+                            // 최소가격
+                            VStack(alignment: .leading, spacing: 14){
+                                Text("최소")
+                                    .font(.custom("ChosunCentennial", size: 24))
+                                
+                                Text("\(tempLowPrice)원")
+                                    .font(.custom("ChosunCentennial", size: 36))
+                                    .background( GeometryReader { geo in
+                                        Path { path in
+                                            path.move(to: CGPoint(x: 0, y: geo.size.height-5))
+                                            path.addLine(to: CGPoint(x: geo.size.width, y: geo.size.height-5))
+                                        }
+                                        .stroke(style: StrokeStyle(lineWidth: 20)) // adjust to your liking
+                                        .foregroundColor(.underBarYellow)
+                                    })
+                                
+                            }
+                            // 최대가격
+                            VStack(alignment: .leading,spacing: 14){
+                                Text("최대")
+                                    .font(.custom("ChosunCentennial", size: 24))
+                                
+                                
+                                Text("\(tempHighPrice)원")
+                                    .font(.custom("ChosunCentennial", size: 36))
+                                    .background( GeometryReader { geo in
+                                        Path { path in
+                                            path.move(to: CGPoint(x: 0, y: geo.size.height-5))
+                                            path.addLine(to: CGPoint(x: geo.size.width, y: geo.size.height-5))
+                                        }
+                                        .stroke(style: StrokeStyle(lineWidth: 20)) // adjust to your liking
+                                        .foregroundColor(.underBarYellow)
+                                    })
+                                
+                            }
                         }
                     }
-                }
-                
-                Spacer(minLength: viewHeight*0.03)//minLength: viewHeight*0.067)
-                
-                
-                // MARK: - 2번째 부분
-                VStack(alignment: .leading, spacing: viewHeight*0.028){
-                    Text("내 물품의 가격은...")
-                        .foregroundColor(.descriptionGray)
-                        .font(.system(size: 16, weight: .medium))
                     
-                    //화살표와
-                    VStack(alignment: .center){
-                        //percentage = (value - min) / (max - min)
-                        let percentage = CGFloat(tempPrice-tempLowPrice)/CGFloat(tempHighPrice-tempLowPrice)
-                        if percentage <= 0.5 {
-                            Triangle()
-                                .fill(Color.underBarGray).opacity(0.5)
-                                .frame(width:29,height: 29)
-                                .offset(x:max(viewWidth*0.91*(percentage-0.5)-15,-viewWidth*0.91*0.5-15))
-                        } else {
-                            Triangle()
-                                .fill(Color.underBarGray).opacity(0.5)
-                                .frame(width:29,height: 29)
+                    Spacer(minLength: viewHeight*0.07)//minLength: viewHeight*0.067)
+                    
+                    
+                    // MARK: - 2번째 부분
+                    VStack(alignment: .leading, spacing: viewHeight*0.002){
+                        Text("내 물품의 가격은...")
+                            .foregroundColor(.descriptionGray)
+                            .font(.system(size: 16, weight: .medium))
+                        
+                        //화살표와
+                        VStack(alignment: .center){
+                            //percentage = (value - min) / (max - min)
+                            let percentage = CGFloat(tempPrice-tempLowPrice)/CGFloat(tempHighPrice-tempLowPrice)
+                            if percentage <= 0.5 {
+                                Triangle()
+                                    .fill(Color.underBarYellow).opacity(0.5)
+                                    .frame(width:29,height: 29)
+                                    .offset(x:max(viewWidth*0.91*(percentage-0.5)-15,-viewWidth*0.91*0.5-15))
+                            } else {
+                                Triangle()
+                                    .fill(Color.triangleYellow)
+                                    .frame(width:29,height: 29)
                                 .offset(x:min(viewWidth*0.91*(percentage-0.5)-15,viewWidth*0.91*0.5-15))                        }
-                        Image("Line")
-                        HStack(alignment: .center, spacing: viewWidth*0.75) {
-                            Text("싼 거")
-                                .font(.custom("ChosunCentennial", size: 14))
-                            Text("비싼 거")
-                                .font(.custom("ChosunCentennial", size: 14))
+                            Image("Line")
+                            HStack(alignment: .center, spacing: viewWidth*0.75) {
+                                Text("싼 거")
+                                    .font(.custom("ChosunCentennial", size: 14))
+                                Text("비싼 거")
+                                    .font(.custom("ChosunCentennial", size: 14))
+                            }
                         }
                     }
-                }
-                Spacer(minLength: viewHeight*0.055)
-                // dfadfdfd
-                // MARK: - 3번째 부분
-                VStack(alignment: .leading, spacing: viewHeight*0.029){
-                    let percentage = CGFloat(tempPrice-tempLowPrice)/CGFloat(tempHighPrice-tempLowPrice)
-                    HStack{
+                    Spacer(minLength: viewHeight*0.055)
+                    // dfadfdfd
+                    // MARK: - 3번째 부분
+                    VStack(alignment: .leading, spacing: viewHeight*0.029){
+                        let percentage = CGFloat(tempPrice-tempLowPrice)/CGFloat(tempHighPrice-tempLowPrice)
+                        HStack{
+                            if percentage >= 0.7 {
+                                Text("에잉 쯧,")
+                                Text("안 사!")
+                                    .background( GeometryReader { geo in
+                                        Path { path in
+                                            path.move(to: CGPoint(x: 0, y: geo.size.height-5))
+                                            path.addLine(to: CGPoint(x: geo.size.width, y: geo.size.height-5))
+                                        }
+                                        .stroke(style: StrokeStyle(lineWidth: 20)) // adjust to your liking
+                                        .foregroundColor(.NopeUnderBarColor)
+                                    })
+                            } else if percentage <= 0.3{
+                                Text("당장")
+                                Text("사자!")
+                                    .background(GeometryReader { geo in
+                                        Path { path in
+                                            path.move(to: CGPoint(x: 0, y: geo.size.height-5))
+                                            path.addLine(to:CGPoint(x: geo.size.width, y: geo.size.height-5))
+                                        }
+                                        .stroke(style: StrokeStyle(lineWidth: 20))
+                                        .foregroundColor(.BuyUnderBarColor)
+                                    })
+                                Text("쟁여!")
+                            } else {
+                                Text("음, 그럭저럭...")
+                            }
+                        }.font(.custom("ChosunCentennial", size: 36))
+                        
                         if percentage >= 0.7 {
-                            Text("에잉 쯧,")
-                            Text("안 사!")
-                                .background( GeometryReader { geo in
-                                    Path { path in
-                                        path.move(to: CGPoint(x: 0, y: geo.size.height-5))
-                                        path.addLine(to: CGPoint(x: geo.size.width, y: geo.size.height-5))
-                                    }
-                                    .stroke(style: StrokeStyle(lineWidth: 20)) // adjust to your liking
-                                    .foregroundColor(.NopeUnderBarColor)
-                                })
+                            Image("nope")
+                                .resizable()
+                                .frame(width: viewWidth*0.91, height: viewHeight*0.42)
+                                .padding(.bottom,50)
                         } else if percentage <= 0.3{
-                            Text("당장")
-                            Text("사자!")
-                                .background(GeometryReader { geo in
-                                    Path { path in
-                                        path.move(to: CGPoint(x: 0, y: geo.size.height-5))
-                                        path.addLine(to:CGPoint(x: geo.size.width, y: geo.size.height-5))
-                                    }
-                                    .stroke(style: StrokeStyle(lineWidth: 20))
-                                    .foregroundColor(.BuyUnderBarColor)
-                                })
-                            Text("쟁여!")
+                            Image("soso")
+                                .resizable()
+                                .frame(width: viewWidth*0.91, height: viewHeight*0.42)
+                                .padding(.bottom,50)
                         } else {
-                            Text("음, 그럭저럭...")
+                            Image("buy")
+                                .resizable()
+                                .frame(width: viewWidth*0.91, height: viewHeight*0.42)
+                                .padding(.bottom,50)
                         }
-                    }.font(.custom("ChosunCentennial", size: 14))
-                    if percentage >= 0.7 {
-                        Image("nope")
-                            .resizable()
-                            .frame(width: viewWidth*0.91, height: viewHeight*0.42)
-                            .padding(.bottom,50)
-                    } else if percentage <= 0.3{
-                        Image("soso")
-                            .resizable()
-                            .frame(width: viewWidth*0.91, height: viewHeight*0.42)
-                            .padding(.bottom,50)
-                    } else {
-                        Image("buy")
-                            .resizable()
-                            .frame(width: viewWidth*0.91, height: viewHeight*0.42)
-                            .padding(.bottom,50)
+                        
                     }
-                    
                 }
             }
-        }.padding(.all)
             
+        }
+            .navigationBarHidden(true)
+        
     }
 }
 
 struct ShowPriceView_Previews: PreviewProvider {
     static var previews: some View {
-        ShowPriceView(tempPrice: 1000)
+        ShowPriceView(tempPrice: 1000, tempLowPrice: 0, tempHighPrice: 0)
     }
 }
 
@@ -173,7 +180,7 @@ extension Color {
     public static var descriptionGray: Color {
         return Color(uiColor: UIColor(red: 149/255, green: 149/255, blue: 149/255, alpha: 1.0))
     }
-    public static var underBarGray: Color {
+    public static var underBarYellow: Color {
         return Color(uiColor: UIColor(red: 232/255, green: 161/255, blue: 0/255, alpha: 0.3))
     }
     public static var barBaseGray: Color {
@@ -190,6 +197,9 @@ extension Color {
     }
     public static var NopeUnderBarColor: Color {
         return Color(uiColor: UIColor(red: 255/255, green: 102/255, blue: 81/255, alpha: 0.3))
+    }
+    public static var triangleYellow: Color {
+        return Color(uiColor: UIColor(red: 232/255, green: 161/255, blue: 0/255, alpha: 0.5))
     }
     
 }
@@ -227,3 +237,35 @@ struct Triangle: Shape {
 }
 
 
+
+//custom navigation bar
+struct CustomNavigationBar: View {
+    
+    let categoryTitle: String
+    
+    @Environment(\.presentationMode) var mode: Binding<PresentationMode>
+    
+    var body: some View {
+        
+        ZStack {
+            HStack {
+                Button(action: {
+                    self.mode.wrappedValue.dismiss()
+                }, label: {
+                    Image(systemName: "chevron.backward")
+                        .font(.system(size: 24,weight: .medium))
+                        .foregroundColor(.black)
+                        .padding(.leading, 8)
+                        .padding([.bottom, .top], 10)
+                })
+                Spacer()
+                Spacer()
+            }
+            
+            Text("\(categoryTitle)")
+                .font(.system(size: 17, weight: .semibold))
+                .padding([.top], 11)
+        }.frame(height: 44)
+        
+    }
+}
